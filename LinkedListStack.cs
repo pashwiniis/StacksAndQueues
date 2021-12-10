de8bbb42e16fb80af9927ec4a02908731c2962b3
pashwiniis <pashwini684@gmail.com>
2021-12-10 15:00:44 +0530

    Created a stack and pushed elements into the stack by using LinkedList

---
+++ LinkedListStack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackAndQueue;
+
+
+namespace StacksAndQueues
+{
+    internal class LinkedListStack
+    {
+        private Node top;
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+
+        //30
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp!=null)
+            {
+                Console.Write(temp.data+" ");
+                temp = temp.next;//1400
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using StacksAndQueues;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***********Welcome to Stack and Queue Program**********");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+        }
+    }
+}
